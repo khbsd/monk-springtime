ca262f759df6f195ac7edf749c27e73e8614c835
Ken Bynell <ken.bynell@gmail.com>
2023-11-15 16:28:05 -0600

    actually fix compatibility framework

--- chauntway/Mods/chauntWay/ScriptExtender/Lua/InitCompatibilityFramework.lua
+++ chauntway/Mods/chauntWay/ScriptExtender/Lua/InitCompatibilityFramework.lua
@@ -3,19 +3,17 @@ subClassGuid = "4a869509-5807-4580-b570-0bf9fc2cc400"
 
 if Ext.Mod.IsModLoaded("67fbbd53-7c7d-4cfa-9409-6d737b4d92a9") then
     local subClasses = {
-      AuthorSubclass = khbsdSpringtime{
+      khbsd_Springtime = {
         modGuid = "d0766f1b-3e74-4442-ba79-8ed733e22976",
         subClassGuid = "4a869509-5807-4580-b570-0bf9fc2cc400",
         class = "monk",
         subClassName = "Springtime"
       }
     }
-  
+
     local function OnStatsLoaded()
-      Mods.SubclassCompatibilityFramework = Mods.SubclassCompatibilityFramework or {}
-      Mods.SubclassCompatibilityFramework.API = Mods.SubclassCompatibilityFramework.Api or {}
-      Mods.SubclassCompatibilityFramework.API.InsertSubClasses(subClasses)
+      Mods.SubclassCompatibilityFramework.Api.InsertSubClasses(subClasses)
     end
-  
+
     Ext.Events.StatsLoaded:Subscribe(OnStatsLoaded)
-  end
+end
