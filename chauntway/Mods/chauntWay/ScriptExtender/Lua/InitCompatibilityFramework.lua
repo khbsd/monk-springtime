c23a597d6d62a24beb4ecaf49bc40cecf2f4bb53
Ken Bynell <ken.bynell@gmail.com>
2023-12-28 23:29:17 -0600

    none basically

--- chauntway/Mods/chauntWay/ScriptExtender/Lua/InitCompatibilityFramework.lua
+++ chauntway/Mods/chauntWay/ScriptExtender/Lua/InitCompatibilityFramework.lua
@@ -12,7 +12,7 @@ if Ext.Mod.IsModLoaded("67fbbd53-7c7d-4cfa-9409-6d737b4d92a9") then
     }
 
     local function OnStatsLoaded()
-      Mods.SubclassCompatibilityFramework.Api.InsertSubClasses(subClasses)
+        Mods.SubclassCompatibilityFramework.Api.InsertSubClasses(subClasses)
     end
 
     Ext.Events.StatsLoaded:Subscribe(OnStatsLoaded)
